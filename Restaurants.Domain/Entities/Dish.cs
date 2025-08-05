a4fd75dacaf78988aefc7597e342fae22d863184
Tomás Delizia <tomidelizia@gmail.com>
2025-08-05 14:37:03 +0200

    Add AddKiloCaloriesToDish migration

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -3,8 +3,9 @@ namespace Restaurants.Domain.Entities;
 public class Dish
 {
     public int Id { get; set; }
-    public Guid RestaurantId { get; set; } 
+    public Guid RestaurantId { get; set; }
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public decimal Price { get; set; }
+    public int? KiloCalories { get; set; }
 }
