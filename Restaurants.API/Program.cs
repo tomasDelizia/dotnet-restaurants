7fcf2ae88969027afb50243c6e2aed0369578544
Tomás Delizia <tomidelizia@gmail.com>
2025-08-05 13:56:04 +0200

    Implement infrastructure service registration and add connection string configuration

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,3 +1,5 @@
+using Restaurants.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// Register the infrastructure services
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Persistance;
+
+namespace Restaurants.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("RestaurantsDb");
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString));
+    }
+}
