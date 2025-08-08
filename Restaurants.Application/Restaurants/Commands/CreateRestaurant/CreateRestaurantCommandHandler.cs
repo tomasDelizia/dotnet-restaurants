d44e2be2391324e8d09dd7a89fb98fbc0ff1528f
Tomás Delizia <tomidelizia@gmail.com>
2025-08-08 10:50:24 +0200

    Refactor CreateRestaurantCommandHandler to associate restaurant with current user as owner

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -3,18 +3,25 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Restaurants.Domain.Repositories;
 using Restaurants.Domain.Entities;
+using Restaurants.Application.Users;
 
 namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 
 public class CreateRestaurantCommandHandler(
     IRestaurantsRepository restaurantsRepository,
+    IUserContext userContext,
     ILogger<CreateRestaurantCommandHandler> logger,
     IMapper mapper) : IRequestHandler<CreateRestaurantCommand, Guid>
 {
     public async Task<Guid> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Creating restaurant {@Restaurant}", request);
+        var currentUser = userContext.GetCurrentUser();
+        logger.LogInformation("Creating restaurant {@Restaurant} for owner {UserName} [{UserId}]",
+            request,
+            currentUser?.Email,
+            currentUser?.Id);
         var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser!.Id;
         var id = await restaurantsRepository.CreateAsync(restaurant);
         return id;
     }
